698d1dede79e395b69fa8b9e4a3b5e438684f70a
152n2 <45387298+152n2@users.noreply.github.com>
2018-12-06 11:09:59 +0900

    Update DK.java

--- DK.java
+++ DK.java
@@ -3,9 +3,10 @@ public class DK{
     int people,i, month, day, LR, BT;
     double height, BMI, weight;
     String A, K,ABO;
-    A = System.console().readLine("入力した数だけ男子高校生のステータスを作ります\n数を入力してね:");
+    A = System.console().readLine("入力した数だけ男子高校生のステータスを作ります\n数を入力してね：");
     people = Integer.parseInt(A);
       for(i = 1 ; i <= people; i++ ){
+        //誕生日を設定
         month = (int)(Math.random() * 12) + 1;
           if ( month == 2 ){
             day = (int)(Math.random() * 28) + 1;
@@ -14,12 +15,14 @@ public class DK{
           } else {
             day = (int)(Math.random() * 31) + 1;
           }
+        //利き手を設定
         LR = (int)(Math.random() * 10) + 1;
           if ( LR == 10 ){
             K = "左";
           } else{
             K = "右";
           }
+        //血液型を設定
         BT = (int)(Math.random() * 10) + 1;
           if ( BT <= 4 ){
             ABO = "A";
@@ -30,9 +33,11 @@ public class DK{
           } else{
             ABO = "AB";
           }
+        //身長　体重を設定
         height = (Math.random() * 0.2) + 1.600;
         BMI = (Math.random() * 6.0) + 18.11;
         weight = (BMI * height * height);
+        //出力
         System.out.println("誕生日 " + month + " 月 " + day + " 日\n" + "利き手 " + K + "\n"+"血液型 "+ ABO + " 型");
         System.out.println("身長 " + (int)(height * 100) + " cm 体重 " + (int)weight +" kg");
         System.out.println("");
